71da647e0b088999ce5beb37fb0af3f7f9eb9a74
Angela Gilhotra <angelagilhotra@gmail.com>
2022-01-21 23:37:43 -0400

    latest changes

--- scripts/configureUniswapV3PriceFeeds.ts
+++ scripts/configureUniswapV3PriceFeeds.ts
@@ -1,10 +1,10 @@
 import { abi as IUniswapV3PoolABI } from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json";
-import { tickToPrice } from "@uniswap/v3-sdk";
-import { Token } from "@uniswap/sdk-core";
+import { Pool, priceToClosestTick, tickToPrice } from "@uniswap/v3-sdk";
+import { CurrencyAmount, Price, Token } from "@uniswap/sdk-core";
 import config from "../config/priceConfig.json";
 import { BigNumber, ethers } from "ethers";
 import minimist from "minimist";
-
+import { network } from "hardhat";
 const provider = new ethers.providers.JsonRpcProvider();
 
 interface Immutables {
@@ -25,29 +25,26 @@ interface State {
   feeProtocol: number;
   unlocked: boolean;
 }
-interface observation {
-  tickCumulatives: [BigNumber, BigNumber];
-  secondsPerLiquidityCumulativeX128s: [BigNumber, BigNumber];
-}
 
-const getPoolState = async (poolContract: ethers.Contract) => {
-  const [liquidity, slot] = await Promise.all([
-    poolContract.liquidity(),
-    poolContract.slot0(),
-  ]);
+const getPoolState = async (poolContract: ethers.Contract, block: string) => {
+  // const [liquidity, slot] = await Promise.all([
+  //   poolContract.liquidity({ blockTag: block }),
+  //   poolContract.slot0({ blockTag: block }),
+  // ]);
+  const slot0Call = await poolContract.slot0();
+  const slot0Raw = await provider.getStorageAt(poolContract.address, 0);
+  return slot0Raw.toString();
 
-  const PoolState: State = {
-    liquidity,
-    sqrtPriceX96: slot[0],
-    tick: slot[1],
-    observationIndex: slot[2],
-    observationCardinality: slot[3],
-    observationCardinalityNext: slot[4],
-    feeProtocol: slot[5],
-    unlocked: slot[6],
-  };
-
-  return PoolState;
+  // const PoolState: State = {
+  //   liquidity: liquidity.toString(),
+  //   sqrtPriceX96: slot[0].toString(),
+  //   tick: slot[1],
+  //   observationIndex: slot[2],
+  //   observationCardinality: slot[3],
+  //   observationCardinalityNext: slot[4],
+  //   feeProtocol: slot[5],
+  //   unlocked: slot[6],
+  // };
 };
 
 const getPoolImmutables = async (poolContract: ethers.Contract) => {
@@ -75,11 +72,6 @@ const getPoolImmutables = async (poolContract: ethers.Contract) => {
 const getPoolContract = (address: string): ethers.Contract =>
   new ethers.Contract(address, IUniswapV3PoolABI, provider);
 
-const getObservation = async (
-  poolContract: ethers.Contract,
-  twap: number
-): Promise<observation> => poolContract.observe([twap, 0]);
-
 const getDecimals = (address: string): Promise<number> =>
   new ethers.Contract(
     address,
@@ -101,67 +93,54 @@ const getDecimals = (address: string): Promise<number> =>
     provider
   ).decimals();
 
-const getPrices = async (poolConfig: any, twap: number): Promise<any> => {
+const setStorage = async (address: string) => {
+  console.log("setting storage");
+  await network.provider.send("hardhat_setStorageAt", [
+    address,
+    "0x0",
+    // change this
+    "0x0000000000000000000000000000000000000000000000000000000000000001",
+  ]);
+};
+
+const getPrices = async (poolConfig: any): Promise<void> => {
+  // const pricePerBlock = poolConfig.pricePerBlock;
   const poolAdress = poolConfig.address;
+
+  // for (const [key, value] of Object.entries(pricePerBlock)) {
+  // const block: string = key;
+  // const quotePrice: number = Number(value);
   const contract = getPoolContract(poolAdress);
-  const state = await getPoolState(contract);
-  const immutables = await getPoolImmutables(contract);
-
-  let tick: number | undefined;
-  if (twap === 0) {
-    tick = state.tick;
-  } else {
-    const { tickCumulatives } = await getObservation(contract, twap);
-    tick =
-      (tickCumulatives[1].toNumber() - tickCumulatives[0].toNumber()) / twap;
-  }
-
-  if (tick === undefined) throw new Error("Unable to create tick");
-
-  const token0Decimals = await getDecimals(immutables.token0);
-  const token1Decimals = await getDecimals(immutables.token1);
-  const token0 = new Token(1, immutables.token0, token0Decimals);
-  const token1 = new Token(1, immutables.token1, token1Decimals);
-
-  const initial = tickToPrice(token0, token1, tick).toSignificant(8);
-
-  const configured = Object.keys(poolConfig.pricePerBlock).map(
-    (block) => poolConfig.pricePerBlock[block]
-  );
-
-  const bumpingTick = Object.keys(poolConfig.pricePerBlock).map((block) =>
-    tickToPrice(
-      token0,
-      token1,
-      tick + poolConfig.pricePerBlock[block]
-    ).toSignificant(8)
-  );
-
-  return {
-    initial,
-    configured,
-    bumpingTick,
-  };
+  console.log("before");
+  console.log({ state: await getPoolState(contract, "latest") });
+  await setStorage(poolAdress);
+  console.log("after");
+  console.log({ state: await getPoolState(contract, "latest") });
+  // const parsed = ethers.utils.parseBytes32String(state);
+  // await contract.slot0()
+  // const immutables = await getPoolImmutables(contract);
+  // const token0Decimals = await getDecimals(immutables.token0);
+  // const token1Decimals = await getDecimals(immutables.token1);
+  // const token0 = new Token(1, immutables.token0, token0Decimals);
+  // const token1 = new Token(1, immutables.token1, token1Decimals);
+  // const tickForPrice = priceToClosestTick(priceToGet);
+  // const { tick } = await getPoolState(contract, block);
+  // const price: string = tickToPrice(token0, token1, tick).toSignificant(8);
+  // console.log({ block, price });
+  // }
 };
 
 const main = async () => {
   const argv = minimist(process.argv.slice(2));
-  if (
-    !argv.token ||
-    typeof argv.token !== "string" ||
-    typeof argv.twap === "boolean"
-  ) {
+  if (!argv.token || typeof argv.token !== "string") {
     throw new Error(
-      "usage: npx ts-node scripts/configureUniswapV3PriceFeeds.ts --token DAI/USDC --twap 2"
+      "usage: npx ts-node scripts/configureUniswapV3PriceFeeds.ts --token DAI/USDC"
     );
   }
   const tokenPair = argv.token.toUpperCase();
-  const twap = Number(argv.twap ?? 0);
   const poolConfig = config.find((o) => o.name === tokenPair);
   if (!poolConfig) throw new Error("Config for pair not found");
-
-  const price = await getPrices(poolConfig, twap);
-  console.log(price);
+  await getPrices(poolConfig);
 };
 
 main().catch((e) => {
---
+++ scripts/test.ts
@@ -0,0 +1,7 @@
+import { ethers } from "ethers";
+
+(() => {
+  console.log(
+    ethers.utils.hexlify("4474364061547331502380220,-195645,68,80,80,0,true")
+  );
+})();
